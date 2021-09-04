b27ba00ad6e9fdb71067173366d3b94a1b8e337f
大津山泰成 <o2yamagym@gmail.com>
2021-09-04 16:29:59 +0900

    整形のロジック改良

--- www/html/ex2/text.php
+++ www/html/ex2/text.php
@@ -4,10 +4,9 @@ $text =  $_POST['text'];
 $text1 = preg_replace("/\r\n|\r|\n| |　/", "", $text);
 $text2 = str_replace('{', " {\n  ", $text1);
 $text3 = str_replace(';', ";\n  ", $text2);
-$text4 = str_replace('}', "}\n\n", $text3);
+$text4 = str_replace('  }', "}\n\n", $text3);
 $text5 = str_replace('*/', "*/\n", $text4);
 $text6 = str_replace(':', ": ", $text5);
 echo str_replace(',', ", ", $text6);
 
 exit;
-?>
