0919ff1078278c1e7237175a105ae03a76e5bf7f
大津山泰成 <o2yamagym@gmail.com>
2021-09-10 19:30:20 +0900

    整形のアルゴリズム改善

--- www/html/ex2/text.php
+++ www/html/ex2/text.php
@@ -7,6 +7,8 @@ $text3 = str_replace(';', ";\n  ", $text2);
 $text4 = str_replace('  }', "}\n\n", $text3);
 $text5 = str_replace('*/', "*/\n", $text4);
 $text6 = str_replace(':', ": ", $text5);
-echo str_replace(',', ", ", $text6);
+$text7 = str_replace(',', ", ", $text6);
+$text8 = str_replace('px', "px ", $text7);
+echo str_replace('#', ' #', $text8);
 
 exit;
